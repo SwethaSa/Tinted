10f74875f5fa65e07352d9079944719624068849
Raveena2030 <123936776+Raveena2030@users.noreply.github.com>
2024-02-09 00:38:42 +0530

    d

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,17 +1,15 @@
 import "./Featured.css";
 import Carousel from "react-bootstrap/Carousel";
 import { useState, useEffect } from "react";
-import * as dotenv from "dotenv";
-
-// dotenv.config();
 
 export default function Featured() {
   let [image, setImage] = useState([]);
 
+  let API = "https://backend-swethasa.vercel.app/products/all";
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const result = await fetch("API");
+        const result = await fetch(API);
         if (!result.ok) {
           throw new Error("Image not found!!");
         }
@@ -27,34 +25,18 @@ export default function Featured() {
   return (
     <>
       <h2>Featured Products</h2>
-      {image.map((imageUrl) => (
-        <img key={imageUrl.idno} src={imageUrl.image1} alt={imageUrl.name} />
-      ))}
-      {/* <Carousel>
-        <Carousel.Item interval={1000}>
-          <ExampleCarouselImage text="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <ExampleCarouselImage text="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <ExampleCarouselImage text="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel> */}
+      <div className="Caro">
+        {image.map((imageUrl, index) => (
+          <Carousel.Item
+            key={imageUrl.idno}
+            interval={index === 0 ? 1000 : 500}
+          >
+            <img src={imageUrl.image1} alt={imageUrl.name} />
+            <p>{imageUrl.name}</p>
+            <p>Rs.{imageUrl.price}</p>
+          </Carousel.Item>
+        ))}
+      </div>
     </>
   );
 }
